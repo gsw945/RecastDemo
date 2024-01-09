84915adb7cac4f253bd89e30ae1d99621bafbbb7
gsw945 <gsw945@foxmail.com>
2024-01-09 21:11:13 +0800

    demo part 2 `pathFindInput()` pass

--- src/inc/demo_part02.h
+++ src/inc/demo_part02.h
@@ -19,6 +19,9 @@ void pathFindInput(dtNavMeshQuery* navQuery, dtQueryFilter* filter) {
 	std::string start;
 	std::cout << "input start point(format: x,y,z, eg: 22,0,34): ";
 	getline(std::cin, start);
+	if (start.length() == 0) {
+		start = "-125330,12127,77526"; // debug quick input
+	}
 	size_t startPosSize = 0;
 	float* startPos = parseInputPoint(start, &startPosSize);
 	if (startPos == nullptr) {
@@ -31,12 +34,16 @@ void pathFindInput(dtNavMeshQuery* navQuery, dtQueryFilter* filter) {
 	dtPolyRef startRef;
 	if (dtStatusFailed(navQuery->findNearestPoly(startPos, extents, filter, &startRef, 0))) {
 		std::cout << "findNearestPoly failed: startPos" << std::endl;
+		delete startPos;
 		return;
 	}
 	
 	std::string end;
 	std::cout << "input end point(format: x,y,z, eg: 22,0,34): ";
 	getline(std::cin, end);
+	if (end.length() == 0) {
+		end = "-123932,11927,74607"; // debug quick input
+	}
 	size_t endPosSize = 0;
 	float* endPos = parseInputPoint(end, &endPosSize);
 	if (endPos == nullptr) {
@@ -47,44 +54,62 @@ void pathFindInput(dtNavMeshQuery* navQuery, dtQueryFilter* filter) {
 	printArray(endPos, endPosSize, ", ");
 
 	dtPolyRef endRef;
-
 	if (dtStatusFailed(navQuery->findNearestPoly(endPos, extents, filter, &endRef, 0))) {
+		delete endPos;
 		std::cout << "findNearestPoly failed: endPos" << std::endl;
 		return;
 	}
 
-	static const int MAX_POLYS = 256;
-
 	// Find path
-	dtPolyRef path[MAX_POLYS];
+	static const int MAX_POLYS = 256;
+	dtPolyRef* path = new dtPolyRef[MAX_POLYS];
 	int pathCount;
-	if (dtStatusFailed(navQuery->findPath(startRef, endRef, startPos, endPos, filter, path, &pathCount, 256))) {
+	if (dtStatusFailed(navQuery->findPath(startRef, endRef, startPos, endPos, filter, path, &pathCount, MAX_POLYS))) {
 		std::cout << "findPath failed" << std::endl;
+		delete startPos;
+		delete endPos;
 		return;
 	}
 	std::cout << "pathCount: " << pathCount << std::endl;
 	if (pathCount < 1) {
+		delete path;
 		return;
 	}
 
 	// Find straight path
-	float* straightPath = nullptr;
+	static const int MAX_POINTS = 256;
+	float* straightPath = new float[MAX_POINTS];
 	int straightPathCount = 0;
-	if (dtStatusFailed(navQuery->findStraightPath(startPos, endPos, path, pathCount, straightPath, 0, 0, &straightPathCount, MAX_POLYS))) {
+	if (dtStatusFailed(navQuery->findStraightPath(startPos, endPos, path, pathCount, straightPath, nullptr, nullptr, &straightPathCount, MAX_POINTS))) {
 		std::cout << "findStraightPath failed" << std::endl;
+		delete startPos;
+		delete endPos;
+		delete path;
 		return;
 	}
 	std::cout << "straightPathCount: " << straightPathCount << std::endl;
-	if (straightPathCount <= 0 || straightPathCount %3 != 0) {
+	if (straightPathCount <= 0 || straightPathCount % 3 != 0) {
 		return;
 	}
 
 	// Copy results
-	for (int i = 0; i < straightPathCount * 3; i++)
+	float** result = new float*[straightPathCount];
+	for (int i = 0; i < straightPathCount * 3; i += 3)
 	{
-		std::cout << i << " -> (" << straightPath[i] << "," << straightPath[i+1] << "," << straightPath[i+2] << ")";
+		result[i / 3] = new float[3] {straightPath[i], straightPath[i + 1], straightPath[i + 2]};
+		std::cout << i / 3 << " -> ";
+		printArray(result[i / 3], 3, ", ");
 	}
-	std::cout << std::endl;
+
+	delete startPos;
+	delete endPos;
+	delete path;
+	delete straightPath;
+	for (int i = 0; i < straightPathCount; i++)
+	{
+		delete result[i];
+	}
+	delete result;
 }
 
 void demoPart02(dtNavMeshQuery* navQuery, dtQueryFilter* filter) {
